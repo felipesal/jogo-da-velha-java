93bd6d456f6b5f98768906f804b3116868887726
Felipe Salles <prof.felipe.salles@gmail.com>
2019-10-17 17:33:49 -0300

    Classes piece, board e boardexception criadas

---
+++ src/jogodavelha/application/Program.java
@@ -0,0 +1,12 @@
+package jogodavelha.application;
+
+import jogodavelha.boardgame.Board;
+import jogodavelha.boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Board board = new Board(3, 3);
+	}
+
+}
